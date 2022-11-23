0afc1719bfd2cd4fbf9af8cde999b937c720404e
adeshrgowda <61423379+adeshrgowda@users.noreply.github.com>
2022-11-23 17:28:39 +0530

    spell

--- user_details.sh
+++ user_details.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "please enter the name of the user to view details"
- USR_NAME="Bharath"
+ USR_NAME="Bharat"
 echo "$USR_NAME"
 NAME=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 1 )
 LAST_NAME=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 2)
