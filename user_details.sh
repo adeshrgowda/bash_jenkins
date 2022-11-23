d112db77b2e36eb23df057f1cc283f31f417a426
adeshrgowda <61423379+adeshrgowda@users.noreply.github.com>
2022-11-23 11:42:20 +0530

    extracting user_details on first name

---
+++ user_details.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+echo "please enter the name of the user to view details"
+read USR_NAME
+echo "$USR_NAME"
+NAME=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 1 )
+LAST_NAME=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 2)
+ADRESS=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 3)
+CONTACT_INF=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 4)
+MAIL=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 5)
+SALARY=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 6)
+echo "NAME=$NAME"
+echo "LAST_NAME=$LAST_NAME"
+echo "ADDRESS=$ADRESS"
+echo "CONTACT_NUMBER=$CONTACT_INF"
+echo "MAIL -ID=$MAIL"
+echo "SALARY=$SALARY"
+#echo "$USR_DETL"
+exit
