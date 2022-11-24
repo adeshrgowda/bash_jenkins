aa94d660395672a3d7621a4e46c5a26842fe8c1b
adeshrgowda <61423379+adeshrgowda@users.noreply.github.com>
2022-11-24 15:09:41 +0530

    storing output file added

--- user_details.sh
+++ user_details.sh
@@ -2,17 +2,18 @@
 echo "please enter the name of the user to view details"
  USR_NAME="$1"
 echo "$USR_NAME"
+rm -rf user_details.txt
 NAME=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 1 )
 LAST_NAME=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 2)
 ADRESS=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 3)
 CONTACT_INF=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 4)
 MAIL=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 5)
 SALARY=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 6)
-echo "NAME=$NAME"
-echo "LAST_NAME=$LAST_NAME"
-echo "ADDRESS=$ADRESS"
-echo "CONTACT_NUMBER=$CONTACT_INF"
-echo "MAIL -ID=$MAIL"
-echo "SALARY=$SALARY"
+echo "NAME=$NAME" >>user_details.txt
+echo "LAST_NAME=$LAST_NAME" >>user_details.txt
+echo "ADDRESS=$ADRESS" >>user_details.txt
+echo "CONTACT_NUMBER=$CONTACT_INF" >>user_details.txt
+echo "MAIL -ID=$MAIL" >>user_details.txt
+echo "SALARY=$SALARY" >>user_details.txt
 #echo "$USR_DETL"
 exit
