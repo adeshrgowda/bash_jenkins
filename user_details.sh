a8f83b78cf68e3791049502a7be92bc17163131c
adeshrgowda <61423379+adeshrgowda@users.noreply.github.com>
2022-11-25 11:05:14 +0530

    deleted storage file

--- user_details.sh
+++ user_details.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 echo "please enter the name of the user to view details"
- USR_NAME="$1"
+ USR_NAME="Adesh"
 echo "$USR_NAME"
-rm -rf user_details.txt
+#rm -rf user_details.txt
 NAME=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 1 )
 LAST_NAME=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 2)
 ADRESS=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 3)
 CONTACT_INF=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 4)
 MAIL=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 5)
 SALARY=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 6)
-echo "NAME=$NAME" >>user_details.txt
-echo "LAST_NAME=$LAST_NAME" >>user_details.txt
-echo "ADDRESS=$ADRESS" >>user_details.txt
-echo "CONTACT_NUMBER=$CONTACT_INF" >>user_details.txt
-echo "MAIL -ID=$MAIL" >>user_details.txt
-echo "SALARY=$SALARY" >>user_details.txt
+echo "NAME=$NAME" 
+echo "LAST_NAME=$LAST_NAME" 
+echo "ADDRESS=$ADRESS" 
+echo "CONTACT_NUMBER=$CONTACT_INF" 
+echo "MAIL -ID=$MAIL" 
+echo "SALARY=$SALARY" 
 #echo "$USR_DETL"
 exit
