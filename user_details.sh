84eece9e958e5453b0db5127b27e57cd4f0d0eab
adeshrgowda <61423379+adeshrgowda@users.noreply.github.com>
2022-11-25 12:39:24 +0530

    added storage file

--- user_details.sh
+++ user_details.sh
@@ -2,6 +2,7 @@
 echo "please enter the name of the user to view details"
  USR_NAME="$1"
 echo "$USR_NAME"
+rm -f user.txt
 #rm -rf user_details.txt
 NAME=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 1 )
 LAST_NAME=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 2)
@@ -9,11 +10,12 @@ ADRESS=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 3)
 CONTACT_INF=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 4)
 MAIL=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 5)
 SALARY=$(cat employs.csv |grep -i "$USR_NAME"|cut -d ',' -f 6)
-echo "NAME=$NAME" 
-echo "LAST_NAME=$LAST_NAME" 
-echo "ADDRESS=$ADRESS" 
-echo "CONTACT_NUMBER=$CONTACT_INF" 
-echo "MAIL -ID=$MAIL" 
-echo "SALARY=$SALARY" 
+echo "NAME=$NAME" >>user.txt
+echo "LAST_NAME=$LAST_NAME" >>user.txt
+echo "ADDRESS=$ADRESS" >>user.txt
+echo "CONTACT_NUMBER=$CONTACT_INF" >>user.txt
+echo "MAIL -ID=$MAIL" >>user.txt
+echo "SALARY=$SALARY" >>user.txt
+echo $?
 #echo "$USR_DETL"
 exit
