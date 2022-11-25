26618359570838e2fa5137d0e2ae7cff51b7e308
adeshrgowda <61423379+adeshrgowda@users.noreply.github.com>
2022-11-25 18:01:56 +0530

    Update sort_user_by_salary.sh

--- sort_user_by_salary.sh
+++ sort_user_by_salary.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 rm -rf salary_list.txt
 echo $?
-cat employs.csv|sed '1d'|awk -F ',' '{if($6>100000)print $1}'>salary_list.txt
+cat employs.csv|sed '1d'|awk -F ',' '{ SALARY=$1 ;if($6>"$SALARY")print $1}'>salary_list.txt
 exit
