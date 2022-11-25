203d9fd49cccfc3ef2a51ff44c921a279589121f
adeshrgowda <61423379+adeshrgowda@users.noreply.github.com>
2022-11-25 16:33:03 +0530

    updated salary var to parameter

--- sort_user_by_salary.sh
+++ sort_user_by_salary.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-cat employs.csv|sed '1d'|awk -F ',' '{if ($6 >= "$1") print $1}'>salary_list.txt
+SALARY="$1"
+cat employs.csv|sed '1d'|awk -F ',' '{if ($6 >= "$SALARY") print $1}'>salary_list.txt
 exit
