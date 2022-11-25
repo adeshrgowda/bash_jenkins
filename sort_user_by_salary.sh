a1b8182ae632a87292e127ed4611d6f89148fff2
adeshrgowda <61423379+adeshrgowda@users.noreply.github.com>
2022-11-25 17:05:30 +0530

    Update sort_user_by_salary.sh

--- sort_user_by_salary.sh
+++ sort_user_by_salary.sh
@@ -2,5 +2,5 @@
 SALARY="$1"
 echo "$SALARY"
 rm -rf salary_list.txt
-cat employs.csv|sed '1d'|awk -F ',' '{if ($6 >= "$SALARY") print $1}'>salary_list.txt
+cat employs.csv|sed '1d'|awk -F ',' '{if($6>="$SALARY")print$1}'>salary_list.txt
 exit
